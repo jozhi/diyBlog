a77679e89dfb47ea2cb63da5e1c111a8568f04e9
jiaoshengzhi <11194720@qq.com>
2020-12-04 00:23:06 +0800

    删除冗余 格式化

--- www.js
+++ www.js
@@ -1,8 +1,7 @@
 const http = require('http');
-
 const PORT = 4003;
-const serverHandle = require('./app');
 
+const serverHandle = require('./app');
 const server= http.createServer(serverHandle);
 server.listen(PORT, ()=>{
   console.log('Server is running on port 4003.');
